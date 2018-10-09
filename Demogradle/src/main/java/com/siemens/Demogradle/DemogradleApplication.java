71178fa9d9d36623a723eb2e1cc893d43176e20f
khyatiyas5 <khyati.vyas@siemens.com>
2018-10-09 14:01:12 +0530

    jenkin demo

---
+++ Demogradle/src/main/java/com/siemens/Demogradle/DemogradleApplication.java
@@ -0,0 +1,12 @@
+package com.siemens.Demogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogradleApplication.class, args);
+	}
+}
